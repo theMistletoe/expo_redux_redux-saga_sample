2cde76a40ec2409b3b1cdecb0cb143b5a0e4fbd8
theMistletoe <katachiwotodomete@gmail.com>
2020-11-05 15:10:33 +0900

    functional componentの場合にはconnectではなくフックで行けるっぽい。 これなら型エラーも起きない

--- Home.tsx
+++ Home.tsx
@@ -1,44 +1,30 @@
-import React, {Component} from 'react'
+import React from 'react'
 import { View, Text, Button } from 'react-native'
-import { connect } from 'react-redux'
 import { setName, deleteName } from './redux'
 import {store} from './redux'
+import { useSelector, useDispatch } from 'react-redux'
 
-export class Home extends Component {
-  render() {
-    return (
-      <View style={{flex: 1, justifyContent: 'space-around', alignItems: 'center'}}>
-        <Text style={{marginTop: 100}}>My name is {this.props.name}.</Text>
-        <View style={{flexDirection: 'row'}}>
-          <Button
-            onPress={this.props.deleteName}
-            title="deleteName"
-          />
-          <Button
-            onPress={() => this.props.setName('カバヤ')}
-            title="setName"
-          />
-        </View>
-        {/* ストアは、以下の様なJSONで記述されています。combine reducerのキーにuserを使ったのでuserプロパティの中に、stateが保存されます。 */}
-        {/* stor のstateを取り出すにはgetStateメソッドを使います。JSON.stringifyで文字列へと変換しています。 */}
-        <Text style={{marginBottom: 100}}>現在のstore: {JSON.stringify(store.getState())}</Text>
+export default function Home() {
+  const getData = useSelector((state: any) => state);
+  const dispatch = useDispatch();
+  console.log(getData);
+
+  return (
+    <View style={{flex: 1, justifyContent: 'space-around', alignItems: 'center'}}>
+      <Text style={{marginTop: 100}}>My name is {getData.user.name}.</Text>
+      <View style={{flexDirection: 'row'}}>
+        <Button
+          onPress={() => dispatch(deleteName())}
+          title="deleteName"
+        />
+        <Button
+          onPress={() => dispatch(setName("カバヤ"))}
+          title="setName"
+        />
       </View>
-    )
-  }
+      {/* ストアは、以下の様なJSONで記述されています。combine reducerのキーにuserを使ったのでuserプロパティの中に、stateが保存されます。 */}
+      {/* stor のstateを取り出すにはgetStateメソッドを使います。JSON.stringifyで文字列へと変換しています。 */}
+      <Text style={{marginBottom: 100}}>現在のstore: {JSON.stringify(store.getState())}</Text>
+    </View>
+  )
 }
-
-const mapStateToProps = state => ({
-  // storeは巨大なJsonの塊なので、nameにjsonから取って来たデータを代入している。 
-  name: state.user.name
-})
-
-const mapDispatchToProps = {
-  // importしたactionCreatorを記述。
-  setName,
-  deleteName
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Home)
